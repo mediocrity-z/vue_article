8344a1b4bc55414ffc6790afcec8183da8556535
祝子莘 <1157023204@qq.com>
2021-09-25 09:33:54 +0800

    初始化了项目

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'element-ui/lib/theme-chalk/index.css';
+// 组件按需导入
+import {Button} from 'element-ui'
 
+Vue.use(Button)
 Vue.config.productionTip = false
 
 new Vue({
